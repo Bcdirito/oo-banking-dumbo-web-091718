13fcd61a19f3ed97587b726469608e3e06adf2ae
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 15:18:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      return 
+      return "Transaction rejected. Please check your account balance."
     end
   end
 end
