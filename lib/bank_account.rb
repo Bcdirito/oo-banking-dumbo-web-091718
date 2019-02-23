a9b5394e0ac9079ac1104aba3c39c455263637e6
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 15:04:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,13 @@
 class BankAccount
   attr_reader :name
-  attr_accessor :balance
+  attr_accessor :balance, :status
   
   @@all = []
   
   def initialize(name)
     @name = name
     @balance = 1000
+    @status = "open"
     @@all << self
   end
 end
