48d9f3b191222eaf5691922f41a171579c8e99dc
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 15:24:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,7 @@ class Transfer
   end
   
   def reverse_transfer
-    binding.pry
-    if @status = "complete"
+    if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
